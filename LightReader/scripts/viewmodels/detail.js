6ed354c784930ff3eb631930d6ab896a71ff6307
jeremy <jeremy@masclaux.net>
2015-05-30 19:00:56 +0200

    Add novel detail page

---
+++ LightReader/scripts/viewmodels/detail.js
@@ -0,0 +1,21 @@
+﻿define(function (require) {
+    var app = require('durandal/app'),
+        ko = require('knockout');
+
+    var activate = function (id) {
+        var model = LightReader.AppModel.Inst();
+        var lightNovel = model.novelList[id];
+
+        this.id(lightNovel.title);
+    }
+
+
+    return {
+        id: ko.observable(),
+        activate: activate,
+    };
+
+
+
+});
+
--- LightReader/scripts/viewmodels/main.js
+++ LightReader/scripts/viewmodels/main.js
@@ -5,6 +5,7 @@
             return router.map([
               { route: '', title: 'Home', moduleId: 'viewmodels/home', nav: true },
               { route: 'list', title: 'List', moduleId: 'viewmodels/list', nav: true },
+              { route: 'detail/:id', title: 'Detail', moduleId: 'viewmodels/detail', nav: true },
               { route: 'reader/:id', title: 'Reader', moduleId: 'viewmodels/reader', nav: true },
             ]).mapUnknownRoutes('hello/index', 'not-found')
               .activate();
