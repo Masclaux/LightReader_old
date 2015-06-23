304876aafa766cc8e7a42ca318a2d4d69d468be2
jeremy.masclaux <info@masclaux.net>
2015-06-23 14:13:50 +0200

    optimize + fastClick

--- LightReader/scripts/viewmodels/home.js
+++ LightReader/scripts/viewmodels/home.js
@@ -1,10 +1,7 @@
 ﻿define(function (require) {
     var app = require('durandal/app'),
-        ko = require('knockout'),
-        jquery = require('jquery'),       
+        ko = require('knockout'),  
         semantic = require('semantic'),
-        Hammer = require('hammer'),
-
         router = require('plugins/router')
     ;
 
@@ -18,26 +15,7 @@
 
     function attached(view)
     {
-        $('.ui.dropdown').dropdown();
-
-        var myElement = $('body');
-
-        // create a simple instance
-        // by default, it only adds horizontal recognizers
-        var mc = new Hammer(myElement.get(0));
-
-        // listen to events...
-        mc.on("press", function (ev) {
-            myElement.textContent = ev.type + " gesture detected.";
-        });
-
-        var mc2 = new Hammer(myElement.get(0));
-
-        // listen to events...
-        mc2.on("tap", function (ev) {
-            myElement.textContent = ev.type + " gesture detected.";
-        });
-
+        $('.ui.dropdown').dropdown();   
     }
 
 
