9c0c99f09a2cb53cb3bbda77867653d5208f5fe6
jeremy <jeremy@masclaux.net>
2015-07-13 14:20:45 +0200

    some tweak
    try to optimize on android

--- LightReader/scripts/appConfig.js
+++ LightReader/scripts/appConfig.js
@@ -17,7 +17,7 @@
 
 define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
     //>>excludeStart("build", true);
-    system.debug(true);
+    system.debug(false);
     //>>excludeEnd("build");
 
     app.title = 'Durandal Starter Kit';
